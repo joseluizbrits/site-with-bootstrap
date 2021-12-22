ef9d474ab91b433f5d51a2fedc92070aef2fd7a5
José Luiz Brits <95440657+joseluizbrits@users.noreply.github.com>
2021-12-22 18:07:31 -0300

    Dia 4 - Mais sobre o useState() e props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import React, { useState } from 'react'
 
+import Nota from './componentes/Nota'
+import Resultado from './componentes/Resutado'
+
+
 export default function App() {
 
-  const [car, setCar] = useState('HRV')
+  const [notas, setNotas] = useState(
+    {
+      nota1: 0,
+      nota2: 0,
+      nota3: 0,
+      nota4: 0
+    }
+  )
 
-  const handleChangeName = (e) => {
-    setCar(e.target.value)
+  const handleSetNota = (e) => {
+    switch (e.target.getAttribute('name')) {
+      case 'nota1':
+        setNotas(
+          {
+            nota1: e.target.value,
+            nota2: notas.nota2,
+            nota3: notas.nota3,
+            nota4: notas.nota4
+          }
+        )
+        break
+      case 'nota2':
+        setNotas(
+          {
+            nota1: notas.nota1,
+            nota2: e.target.value,
+            nota3: notas.nota3,
+            nota4: notas.nota4
+          }
+        )
+        break
+      case 'nota3':
+        setNotas(
+          {
+            nota1: notas.nota1,
+            nota2: notas.nota2,
+            nota3: e.target.value,
+            nota4: notas.nota4
+          }
+        )
+        break
+      case 'nota4':
+        setNotas(
+          {
+            nota1: notas.nota1,
+            nota2: notas.nota2,
+            nota3: notas.nota3,
+            nota4: e.target.value
+          }
+        )
+        break
+    }
   }
 
   return (
     <>
-      <label>Selecione um carro</label>
-      <select value={car} onChange={(e) => handleChangeName(e)}>
-        <option value='HRV'>HRV</option>
-        <option value='Golf'>Golf</option>
-        <option value='Sonata'>Sonata</option>
-        <option value='Argo'>Argo</option>
-      </select>
-      <p>Carro selecionado: {car}</p>
+      <Nota num={1} nome={'nota1'} nota={notas.nota1} setNota={handleSetNota}/>
+      <Nota num={2} nome={'nota2'} nota={notas.nota2} setNota={handleSetNota}/>
+      <Nota num={3} nome={'nota3'} nota={notas.nota3} setNota={handleSetNota}/>
+      <Nota num={4} nome={'nota4'} nota={notas.nota4} setNota={handleSetNota}/>
+      <Resultado somaNotas={parseFloat(notas.nota1) + parseFloat(notas.nota2) + parseFloat(notas.nota3) + parseFloat(notas.nota4)}/>
     </>
   )
 }
