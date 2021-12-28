59588bcf9a1daa974bd937447f8199c1f63e671b
José Luiz Brits <95440657+joseluizbrits@users.noreply.github.com>
2021-12-28 17:36:22 -0300

    Dia 7 - Ainda praticando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ import BoxInsert from './componentes/BoxInsert'
 
 export default function App() {
 
-  let [box, setBox] = useState('')
+  const [boxInsert, setBoxInsert] = useState([])
+
+  const [courses, setCourses] = useState(
+    [
+      {
+        courseName: '',
+        fta: false,
+        a1: false,
+        a2: false
+      }
+    ]
+  )
+
+  console.log(courses[0].courseName)
 
   return (
     <>
@@ -16,12 +29,11 @@ export default function App() {
         <div className='status--course'>
           <StatusCourse />
         </div>
-        <button className='insert--button' onClick={() => setBox(box = <BoxCurse />)}>Inserir disciplina</button>
+        <button className='insert--button' onClick={() => setBoxInsert([<BoxInsert courses={courses}/>])}>Inserir disciplina</button>
       </header>
       <div className='window--course'>
-        {box}
+        {boxInsert[0]}
       </div>
-      {/* <BoxInsert /> */}
     </>
   )
 }
--- src/componentes/BoxInsert.js
+++ src/componentes/BoxInsert.js
@@ -1,18 +1,20 @@
 import react from 'react'
 import './BoxInsert.css'
 
-export default function BoxInsert() {
+export default function BoxInsert({ courses }) {
+
     return (
         <div className="box--insert">
             <label>Disciplina</label>
-            <input type='text' placeholder='Inserir disciplina...'></input>
+            <input type='text' placeholder='Inserir disciplina...' value={courses[0].courseName}></input>
             <label>Tarefas</label>
-            <input type='checkbox' checked='checked' id='check_fta' value=''></input>
-            <label for='check_fta'>FTA</label>
-            <input type='checkbox' id='check_fta' value=''></input>
-            <label for='check_fta'>AIA (A1)</label>
-            <input type='checkbox' id='check_fta' value=''></input>
-            <label for='check_fta'>AIA (A2)</label>
+            <input type='checkbox' checked='checked' id='check_fta' value={courses[0].fta}></input>
+            <label htmlFor='check_fta'>FTA</label>
+            <input type='checkbox' id='check_a1' value={courses[0].a1}></input>
+            <label htmlFor='check_a1'>AIA (A1)</label>
+            <input type='checkbox' id='check_a2' value={courses[0].a2}></input>
+            <label htmlFor='check_a2'>AIA (A2)</label>
+            <button onClick=''>Inserir</button>
         </div>
     )
 }
