cbe3785b2be65175365f619ae40a455f39b7f742
José Luiz Brits <95440657+joseluizbrits@users.noreply.github.com>
2022-01-06 14:07:49 -0300

    Dia 11 - Exemplo de Calculadora

--- src/App.js
+++ src/App.js
@@ -1,98 +1,148 @@
 import React, { useState } from 'react'
 
-const tabelaIMC = () => {
-  return (
-    <table border='1' style={{borderCollapse: 'collapse'}}>
-      <thead>
-        <tr>
-          <th>Classificação</th>
-          <th>IMC</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Abaixo do peso</td>
-          <td>Abaixo de 18,5</td>
-        </tr>
-        <tr>
-          <td>Peso normal</td>
-          <td>Entre 18,5 e 24,9</td>
-        </tr>
-        <tr>
-          <td>Sobrepeso</td>
-          <td>Entre 25 e 29,9</td>
-        </tr>
-        <tr>
-          <td>Obesidade Grau I</td>
-          <td>Entre 30 e 34,9</td>
-        </tr>
-        <tr>
-          <td>Obesidade Grau II</td>
-          <td>Entre 35 e 39,9</td>
-        </tr>
-        <tr>
-          <td>Obesidade Grau III</td>
-          <td>Maior que 40</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-const componentePeso = (p, setP) => {
-  return (
-    <div>
-      <label>
-        Peso
-        <input value={p} onChange={(e) => setP(e.target.value)}></input>
-      </label>
-    </div>
-  )
-}
-
-const componenteAltura = (a, setA) => {
-  return (
-    <div>
-      <label>
-        Altura
-        <input value={a} onChange={(e) => setA(e.target.value)}></input>
-      </label>
-    </div>
-  )
-}
-
-const componenteCalcular = (p, a, set) => {
-  const calcular = () =>{
-    set(p / (a * a))
-  }
-  return (
-    <div>
-      <button onClick={calcular}>Calcular</button>
-    </div>
-  )
-}
-
-const componenteResultado = (res) => {
-  return (
-    <div>
-      <p>Resultado: {res.toFixed(2)}</p>
-    </div>
-  )
-}
-
 export default function App() {
-
-  const [peso, setPeso] = useState(0)
-  const [altura, setAltura] = useState(0)
+  
+  const [valorTela, setValorTela] = useState('')
   const [resultado, setResultado] = useState(0)
+  const [acumulador, setAcumulador] = useState(0)
+  const [operado, setOperado] = useState(false)
+
+  // COMPONENTES
+  const Tela = (valor, res) => {
+    return (
+      <div style={cssTela}>
+        <span style={cssTelaOperacao}>{valor}</span>
+        <span style={cssTelaResultado}>{res}</span>
+      </div>
+    )
+  }
+
+  const Butaum = (label, onClick) => {
+    return (
+      <button style={cssButaum} onClick={onClick}>{label}</button>
+    )
+  }
+
+  // FUNÇÕES
+  const addDigitoTela = (d) => {
+    if ((d === '+' || d === '-' || d === '*' || d === '/') && operado) {
+      setOperado(false)
+      setValorTela(resultado + d)
+      return
+    }
+    if (operado) {
+      setValorTela(d)
+      setOperado(false)
+      return
+    }
+    const ValorDigitadoTela = valorTela + d
+    setValorTela(ValorDigitadoTela)
+  }
+
+  const limparMemoria = () => {
+    setOperado(false)
+    setValorTela('')
+    setResultado(0)
+    setAcumulador(0)
+    return
+  }
+
+  const Operacao = (oper) => {
+    if (oper === 'bs') {
+      let vtela = valorTela
+      vtela = vtela.substring(0, (vtela.length - 1))
+      setValorTela(vtela)
+      setOperado(false)
+      return
+    }
+    try {
+      const resCalculo = eval(valorTela)
+      setAcumulador(resCalculo)
+      setResultado(resCalculo)
+      setOperado(true)
+    }
+    catch {
+      setResultado('ERRO')
+    }
+  }
+
+  // ESTILOS
+  const cssConteiner = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    width: 300,
+    border: '1px solid #000'
+  }
+
+  const cssButauns = {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+
+  const cssTela = {
+    display: 'flex',
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#444',
+    flexDirection: 'column',
+    width: 260
+  }
+
+  const cssTelaOperacao = {
+    fontSize: 25,
+    color: '#fff',
+    height: 20
+  }
+
+  const cssTelaResultado = {
+    fontSize: 50,
+    color: '#fff',
+  }
+
+  const cssButaum = {
+    fontSize: 30,
+    height: 75,
+    width: 75,
+    padding: 20,
+    backgroundColor: '#000',
+    color: '#fff',
+    borderColor: '#000',
+    textAlign: 'center',
+    outline: 'none'
+  }
 
   return (
     <>
-      {componentePeso(peso, setPeso)}
-      {componenteAltura(altura, setAltura)}
-      {componenteCalcular(peso, altura, setResultado)}
-      {componenteResultado(resultado)}
-      {tabelaIMC()}
+      <div style={cssConteiner}>
+        <h3>Calculadora Matemática Simples</h3>
+        {Tela(valorTela, resultado)}
+        <div style={cssButauns}>
+          {Butaum('AC', limparMemoria)}
+          {Butaum('(', () => addDigitoTela('('))}
+          {Butaum(')', () => addDigitoTela(')'))}
+          {Butaum('/', () => addDigitoTela('/'))}
+          {Butaum('7', () => addDigitoTela('7'))}
+          {Butaum('8', () => addDigitoTela('8'))}
+          {Butaum('9', () => addDigitoTela('9'))}
+          {Butaum('*', () => addDigitoTela('*'))}
+          {Butaum('4', () => addDigitoTela('4'))}
+          {Butaum('5', () => addDigitoTela('5'))}
+          {Butaum('6', () => addDigitoTela('6'))}
+          {Butaum('-', () => addDigitoTela('-'))}
+          {Butaum('1', () => addDigitoTela('1'))}
+          {Butaum('2', () => addDigitoTela('2'))}
+          {Butaum('3', () => addDigitoTela('3'))}
+          {Butaum('+', () => addDigitoTela('+'))}
+          {Butaum('0', () => addDigitoTela('0'))}
+          {Butaum('.', () => addDigitoTela('.'))}
+          {Butaum('<-', () => Operacao('bs'))}
+          {Butaum('=', () => Operacao('='))}
+        </div>
+      </div>
     </>
   )
 }
